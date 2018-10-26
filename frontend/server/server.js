88693b0ac42cab0d40366bb5fb5bc239dabb31fe
Eric Zhao <zhao.yichao15@gmail.com>
2018-10-26 12:46:04 +1100

    v2 - docker build

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -5,9 +5,11 @@ const bodyParser = require("body-parser");
 const useragent = require("express-useragent");
 const compression = require( "compression");
 const cookieParser = require( "cookie-parser");
-const port = process.env.PORT || 3000;
-const backendHost = process.env.BACKEND_HOST || 'http://localhost:5000'
+const port = 3000;
+const backendHost = process.env.BACKEND_ENDPOINT || 'http://localhost:5000'
 const app = express()
+
+console.log(backendHost)
 
 app.use(useragent.express());
 app.use(compression());
@@ -17,8 +19,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/home/', (req, res)=>{
     axios.get(`${backendHost}/api/home`).then(response=>{
+        console.log(`${backendHost}/api/home`)
+        console.log(response.data)
         res.status(200).json(response.data)
     }).catch(err=>{
+        console.log(err)
         res.send(err)
     })
 })
